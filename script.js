c46fb5b5a74a2eaddd09d84c3e5791bfe051ccf4
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-12-18 11:23:56 +0800

    A

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const label = document.querySelector(".label");
 const image = document.querySelector(`.images`);
 const text = document.querySelector(`.text`);
 const allBtns = document.querySelectorAll(`.image`);
+const history = document.getElementById(`history`);
+const vision = document.getElementById(`vision`);
+const goals = document.getElementById(`goals`);
 
 const historyBtn = () => {
   buttons(0);
@@ -21,18 +24,27 @@ const buttons = (index) => {
       image.classList.add(`image1`);
       image.classList.remove(`image2`);
       image.classList.remove(`image3`);
+      history.classList.add(`background`);
+      vision.classList.remove(`background`);
+      goals.classList.remove(`background`);
       label.textContent = `History`;
       text.style.color = `#ff853f`;
     } else if (index === 1) {
       image.classList.remove(`image1`);
       image.classList.add(`image2`);
       image.classList.remove(`image3`);
+      history.classList.remove(`background`);
+      vision.classList.add(`background`);
+      goals.classList.remove(`background`);
       label.textContent = `Vision`;
       text.style.color = `#96bbab`;
     } else if (index === 2) {
       image.classList.remove(`image1`);
       image.classList.remove(`image2`);
       image.classList.add(`image3`);
+      history.classList.remove(`background`);
+      vision.classList.remove(`background`);
+      goals.classList.add(`background`);
       label.textContent = `Goals`;
       text.style.color = `#79c0d7`;
     }
